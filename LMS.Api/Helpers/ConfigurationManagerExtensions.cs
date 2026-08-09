e9499af13497be903124bf2268ed3d4d1ed8f0b1
raveendrahewage <hewagerv@gmail.com>
2026-08-09 10:19:18 +0530

    Improve env var check; remove WriteConfigurationSources

--- LMS.Api/Helpers/ConfigurationManagerExtensions.cs
+++ LMS.Api/Helpers/ConfigurationManagerExtensions.cs
@@ -12,24 +12,11 @@ namespace LMS.Api.Helpers
         {
             string keyVaultEndpoint = Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
 
-            if (keyVaultEndpoint is null)
+            if (string.IsNullOrWhiteSpace(keyVaultEndpoint))
                 throw new InvalidOperationException("Store the Key Vault endpoint in a KEYVAULT_ENDPOINT environment variable.");
 
             SecretClient secretClient = new(new Uri(keyVaultEndpoint), new DefaultAzureCredential());
             config.AddAzureKeyVault(secretClient, new AzureKeyVaultConfigurationOptions());
-        }
-
-        public static void WriteConfigurationSources(this ConfigurationManager config)
-        {
-            Console.WriteLine("Configuration sources\n=====================");
-            foreach (var source in config.Sources)
-            {
-                if (source is JsonConfigurationSource jsonSource)
-                    Console.WriteLine($"{source}: {jsonSource.Path}");
-                else
-                    Console.WriteLine(source.ToString());
-            }
-            Console.WriteLine("=====================\n");
         }
     }
 }
