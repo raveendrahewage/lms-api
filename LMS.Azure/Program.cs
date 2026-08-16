72fcb7acd429d22923dac75215b7fb2d82c9a688
raveendrahewage <hewagerv@gmail.com>
2026-08-16 21:42:58 +0530

    Remove Azure Key Vault integration for secrets

--- LMS.Azure/Program.cs
+++ LMS.Azure/Program.cs
@@ -1,30 +1,15 @@
-using Azure.Security.KeyVault.Secrets;
 using Azure.Storage.Blobs;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using DefaultAzureCredential = Azure.Identity.DefaultAzureCredential;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-var keyVaultUrl = Environment.GetEnvironmentVariable("KEYVAULT_ENDPOINT");
+string blobConnectionString = builder.Configuration["BlobStorageConnection"]
+    ?? throw new InvalidOperationException("BlobStorageConnection is missing from configuration.");
 
-if (!string.IsNullOrEmpty(keyVaultUrl))
-{
-    // Production / Staging: Read secrets via Key Vault & Managed Identity
-    var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
-    builder.Services.AddSingleton(secretClient);
-
-    var blobConnectionString = secretClient.GetSecret("ConnectionStrings:BlobStorageConnection").Value.Value;
-    builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
-}
-else
-{
-    var blobConnectionString = Environment.GetEnvironmentVariable("ConnectionStrings:BlobStorageConnection")!;
-    builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
-}
+builder.Services.AddSingleton(new BlobServiceClient(blobConnectionString));
 
 builder.Build().Run();
